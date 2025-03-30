2ca50ba34a665a606191a124f5a4f17728a29482
Raphael Rossi <me@raphaelrossi.io>
2025-03-30 13:21:19 -0300

    chore: change default ssh-agent timeout to 300 seconds (5 minutes)

--- .config/fish/conf.d/ssh-agent.fish
+++ .config/fish/conf.d/ssh-agent.fish
@@ -5,5 +5,5 @@ set SSH_AUTH_SOCK $HOME/.ssh/ssh-agent.sock
 
 ssh-add -l 2>/dev/null >/dev/null
 if test $status -ge 2
-  eval (ssh-agent -a "$SSH_AUTH_SOCK" -c -t 20)
+  eval (ssh-agent -a "$SSH_AUTH_SOCK" -c -t 300)
 end
