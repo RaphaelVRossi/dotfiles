100a7afc0cf2c64bda31e983774dd82fbad2ab81
Raphael Rossi <raphael.rossi@stone.com.br>
2025-06-02 14:02:01 -0300

    feat: create autocmds file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
 require("config.lazy")
 require("config.keymap")
 require("config.options")
-
+require("config.autocmds")
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,53 +1,67 @@
-local lazy = require('legendary.toolbox').lazy
-require('legendary').autocmds({
-	{
-		name = 'Custom',
-		clear = true,
-		{ 'FocusLost', 'silent! wa' },
-		{ 'TextYankPost', lazy(vim.highlight.on_yank, { higroup = 'TextYank', timeout = 150 }) },
-		{ 'VimResized', lazy(vim.api.nvim_command, 'wincmd =') },
-		{
-			{ 'BufEnter', 'BufWritePost' },
-			function(args)
-				local bufinfo = vim.fn.getbufinfo(args.buf)
-				if #bufinfo ~= 1 then
-					return
-				end
-
-				local buffer = bufinfo[1]
-				if buffer.listed == 0 then
-					return
-				end
-
-				local name = ''
-				local directory = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-				if buffer.name == '' then
-					name = '[No Name]'
-				else
-					name = vim.fn.fnamemodify(buffer.name, ':t')
-				end
-				vim.opt.titlestring = string.format('%s - %s', name, directory)
-			end,
-		},
-	},
-})
-
 vim.api.nvim_create_autocmd('BufRead', {
-  callback = function(opts)
-    vim.api.nvim_create_autocmd('BufWinEnter', {
-      once = true,
-      buffer = opts.buf,
-      callback = function()
-        local ft = vim.bo[opts.buf].filetype
-        local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
-        if
-          not (ft:match('commit') and ft:match('rebase'))
-          and last_known_line > 1
-          and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
-        then
-          vim.api.nvim_feedkeys([[g`"]], 'x', false)
-        end
-      end,
-    })
-  end,
+    callback = function(opts)
+        vim.api.nvim_create_autocmd('BufWinEnter', {
+            once = true,
+            buffer = opts.buf,
+            callback = function()
+                local ft = vim.bo[opts.buf].filetype
+                local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+                if
+                    not (ft:match('commit') and ft:match('rebase'))
+                    and last_known_line > 1
+                    and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+                then
+                    vim.api.nvim_feedkeys([[g`"]], 'x', false)
+                end
+            end,
+        })
+    end,
 })
+
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "TelescopePreviewerLoaded",
+    callback = function(args)
+        vim.wo.number = true
+        vim.wo.wrap = false
+    end,
+})
+
+require('lspconfig').lua_ls.setup { -- Or lspconfig.sumneko_lua.setup if you're using an older name
+    settings = {
+        Lua = {
+            -- Make the server aware of Neovim runtime files
+            runtime = {
+                -- Tell the language server which version of Lua you're using (Neovim uses LuaJIT)
+                version = 'LuaJIT',
+                -- Setup your lua path as per your OS and Neovim setup
+                -- path = vim.split(package.path, ';'), -- You might not need this if using 'workspace.library'
+            },
+            diagnostics = {
+                -- Get rid of warnings about undefined globals
+                globals = { 'vim' }, -- Add 'vim' here
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                -- or specifically for Neovim Lua development:
+                -- library = {
+                --   [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                --   [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                --   -- Add any other paths to your Neovim Lua plugins or stdpaths
+                --   -- [vim.fn.stdpath('config') .. '/lua'] = true,
+                -- },
+                checkThirdParty = false, -- Can sometimes help, but 'library' is more direct
+            },
+            -- This is deprecated but some older configs might use it.
+            -- It's better to use 'workspace.library' and 'diagnostics.globals'.
+            -- completion = {
+            --   callSnippet = "Replace"
+            -- },
+            -- telelemetry = {
+            --   enable = false,
+            -- },
+        },
+    },
+    -- on_attach, capabilities, etc. as you have them
+}
